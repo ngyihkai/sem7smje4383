aa78163a4f16684e865015b10026a989ce8ed670
ngyihkai <ngyihkai12@gmail.com>
2022-01-26 15:52:39 +0800

    parallelsys

---
+++ CH6/sort.py
@@ -0,0 +1,17 @@
+my_list = list('xyzabc')
+print(my_list)
+my_list.sort() # no return
+print(my_list)
+
+my_list = 'this is a test'.split() #split into each word only
+print(my_list)
+my_list = list('this is a test') #list each char only
+print(my_list)
+my_list = sorted('this is a test') #sort string only
+print(my_list)
+my_list1 = 'this is a test'
+string_elements=my_list1.split() #split into each word only
+reversed_elements = []
+for element in string_elements:
+    string_elements.append(element[::-1])
+print(reversed_elements)
---
+++ CH6/split.py
@@ -0,0 +1,2 @@
+split_list = 'this is a test'.split()
+print (split_list)
